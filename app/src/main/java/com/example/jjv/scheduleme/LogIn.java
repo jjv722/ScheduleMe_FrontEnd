914f1ae43cddfd11304692c4a68c8099439569f0
Mauricio Amezcua <mamezcua94@gmail.com>
2016-11-06 15:21:48 -0600

    added progress bar

--- app/src/main/java/com/example/jjv/scheduleme/LogIn.java
+++ app/src/main/java/com/example/jjv/scheduleme/LogIn.java
@@ -1,13 +1,17 @@
 package com.example.jjv.scheduleme;
 
 import android.content.Context;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONObject;
 
@@ -17,6 +21,10 @@ public class LogIn extends AppCompatActivity implements Async_API.callback {
     EditText email;
     EditText password;
     Button logIn;
+    ProgressBar loading;
+    LinearLayout signIn;
+    String SERVER_URL = "http://192.168.0.4:8000/";
+    Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +34,8 @@ public class LogIn extends AppCompatActivity implements Async_API.callback {
         email = (EditText) findViewById(R.id.email);
         password = (EditText) findViewById(R.id.password);
         logIn = (Button) findViewById(R.id.logIn);
+        loading = (ProgressBar) findViewById(R.id.loading);
+        signIn = (LinearLayout) findViewById(R.id.sign_in);
 
         logIn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,30 +46,47 @@ public class LogIn extends AppCompatActivity implements Async_API.callback {
     }
 
     public void sendLoginInfo(){
-        JSONObject params = new JSONObject();
-        try{
+        try {
+            JSONObject params = new JSONObject();
             params.put("u", email.getText().toString());
             params.put("p", password.getText().toString());
-            URL loginURL = new URL("http://192.168.0.4:8000/api/login");
+            URL loginURL = new URL(SERVER_URL + "api/login");
             new Async_API(this, params).execute(loginURL);
-        }
-        catch(Exception e){
-
-        }
+        } catch(Exception e) {}
     }
 
     @Override
     public void start() {
-
+        loading.setVisibility(View.VISIBLE);
+        signIn.setVisibility(View.GONE);
     }
 
     @Override
-    public void success(JSONObject jsonObject) {
-        Log.d("success:", jsonObject.toString());
+    public void success(final JSONObject jsonObject) {
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                loading.setVisibility(View.GONE);
+
+                Log.d("success:", jsonObject.toString());
+                Toast.makeText(
+                        getApplicationContext(),
+                        "Log in successful!" + jsonObject.toString(),
+                        Toast.LENGTH_SHORT
+                ).show();
+            }
+        }, 3000);
     }
 
     @Override
     public void error() {
-
+        loading.setVisibility(View.GONE);
+        signIn.setVisibility(View.VISIBLE);
+        // there was a problem contacting the server...
+        Toast.makeText(
+                getApplicationContext(),
+                "There was a problem when trying to log in...",
+                Toast.LENGTH_SHORT
+        ).show();
     }
 }
