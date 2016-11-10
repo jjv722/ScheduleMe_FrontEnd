c99e6556d2cdb3a5b5ca92b99dacefefc59e3e9a
Juan Villegas <jjv@wireless-10-147-20-145.public.utexas.edu>
2016-11-10 10:42:42 -0600

    Working on BookNow

---
+++ app/src/main/java/com/example/jjv/scheduleme/BookNow/BookNowFragment.java
@@ -0,0 +1,74 @@
+package com.example.jjv.scheduleme.BookNow;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.jjv.scheduleme.Async_API;
+import com.example.jjv.scheduleme.Auth.LogMe;
+import com.example.jjv.scheduleme.Auth.RegisterFragment;
+import com.example.jjv.scheduleme.R;
+
+import org.json.JSONObject;
+
+/**
+ * Created by jjv on 11/9/16.
+ */
+
+public class BookNowFragment extends Fragment {
+    final String[] cost = {"$", "$$", "$$$", "$$$$"};
+    final String[] ranges = {"5 miles", "10 miles", "15 miles", "20 miles"};
+    final String[] categories = {"General Practice", "Optometry", "Dentistry", "Pediatrics"};
+    Spinner maxCost;
+    Spinner maxRange;
+    Spinner category;
+    EditText address;
+    EditText date;
+
+    Button book;
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_book_now, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        maxCost = (Spinner) view.findViewById(R.id.priceRange);
+        maxRange= (Spinner) view.findViewById(R.id.distanceRange);
+        category= (Spinner) view.findViewById(R.id.category);
+        address = (EditText) view.findViewById(R.id.address);
+        date = (EditText) view.findViewById(R.id.date);
+        book = (Button) view.findViewById(R.id.book);
+
+        book.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v != null) {
+                    InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+
+            }
+        });
+
+
+    }
+
+
+}
--- app/src/main/java/com/example/jjv/scheduleme/Main_Menu.java
+++ app/src/main/java/com/example/jjv/scheduleme/Main_Menu.java
@@ -2,6 +2,7 @@ package com.example.jjv.scheduleme;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
@@ -9,17 +10,14 @@ import android.widget.Toast;
  * Created by jjv on 11/9/16.
  */
 
-public class Main_Menu extends AppCompatActivity{
+public class Main_Menu extends FragmentActivity {
 
     public void onCreate(Bundle bundle){
         super.onCreate(bundle);
         setContentView(R.layout.main_menu);
         Intent intent = getIntent();
-        Toast.makeText(
-                this,
-                "Inside MainActivity",
-                Toast.LENGTH_SHORT
-        ).show();
+
+
 
 
 
